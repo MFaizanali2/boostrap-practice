0e3f03f7df381c8f5c92596534e1d694aad20002
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-01 19:16:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+let box = document.querySelector(".box")
+
+box.innerHTML = `
+<div id="container">
+            <div id="navbar">
+                <h1>My potfolio</h1>
+                <div id="nav">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Service</a></li>
+                        <li><a href="#">Contract</a></li>
+                        <li><a href="#">My work</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="second">
+
+        </div>
+
+        <div id="third">
+            
+        </div>`
+
 window.addEventListener("scroll", function(){
     let nav = document.querySelector("#navbar");
     nav.classList.toggle("sticky", window.scrollY > 0);
